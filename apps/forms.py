103157eb40266936ff94aa5133d44647a31c5eef
xolmomin <xolmomin@gmail.com>
2023-03-13 17:13:15 +0500

    add forms

--- apps/forms.py
+++ apps/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 from apps.models import Contact
 
@@ -6,9 +7,31 @@ from apps.models import Contact
 # form, modelform
 
 class ContactForm(forms.ModelForm):
-    email = forms.EmailField()
-    password = forms.CharField(max_length=255)
-    address = forms.CharField(max_length=255)
+    email = forms.EmailField(required=True)
+    password = forms.CharField(max_length=255, required=True)
+    confirm_password = forms.CharField(max_length=255, required=True)
+    address = forms.CharField(max_length=255, required=True)
+
+    def clean_email(self):
+        value = self.cleaned_data.get('email')
+        if len(value) <= 5:
+            raise ValidationError('Please enter valid email address!')
+        return value
+
+    def clean_address(self):
+        value = self.cleaned_data.get('address')
+        if value != 'Toshkent':
+            raise ValidationError('Please enter Tashkent')
+        return value
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.pop('confirm_password')
+        if confirm_password != password:
+            raise ValidationError("Password didn't match!")
+        return cleaned_data
 
     class Meta:
         model = Contact
+        fields = ('email', 'password', 'address')
--- apps/views.py
+++ apps/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
+from apps.forms import ContactForm
 
 
 def index_page(request):
+    context = {
+        'form': ContactForm()
+    }
     if request.method == 'POST':
-        pass
-    return render(request, 'index.html')
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+        context = {
+            'form': form
+        }
+    return render(request, 'index.html', context)
 
 
 def menu_page(request):
